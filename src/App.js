a234aa8bc8f0cf610637f8297b76724ee46cc11c
Philip Read <Philip.Read.000@gmail.com>
2018-03-22 22:51:37 -0500

    Add board sizes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import Square from './Square.js';
 import './App.css';
 
+let boardClasses = {
+  4 : 'four-by-four',
+  6 : 'six-by-six',
+  8 : 'eight-by-eight',
+  10 : 'ten-by-ten',
+  12 : 'twelve-by-twelve'
+}
+
+let gameSize = 12;
+
 class App extends Component {
   render() {
     let squareArr = [];
-    for(let i = 0; i<16; i++) {
+    for(let i = 0; i<(gameSize*gameSize); i++) {
       squareArr.push(<Square key={i} squareState='empty'/>);
     }
     return (
-      <div className="container">
+      <div className={`container ${boardClasses[gameSize]}`}>
         {squareArr}
       </div>
     );
