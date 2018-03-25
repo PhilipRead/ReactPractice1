06a6503711a8c5791178218a9bcedf26523e125f
Philip Read <Philip.Read.000@gmail.com>
2018-03-25 14:12:47 -0500

    Add base board status logic

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,24 +3,54 @@ import Square from '../Square/Square.js';
 import './Board.css';
 
 let boardClasses = {
-    4 : 'four-by-four',
-    6 : 'six-by-six',
-    8 : 'eight-by-eight',
-    10 : 'ten-by-ten',
-    12 : 'twelve-by-twelve'
-  }
+  4 : 'four-by-four',
+  6 : 'six-by-six',
+  8 : 'eight-by-eight',
+  10 : 'ten-by-ten',
+  12 : 'twelve-by-twelve'
+}
 
 class Board extends Component {
     constructor(props) {
         super(props);
-        this.state = { gameSize : props.gameSize };
+        this.gameSize = props.gameSize;
+        this.unmarkedSquares = this.gameSize * this.gameSize;
+        this.board = [];
+    }
+
+    checkIfSolved() {
+      console.log('checking if solved');
+    }
+    
+    updateBoard = (i, k, value) => {
+      let curValue = this.board[i][k];
+      if(curValue === value) {
+        return;
+      } else if (curValue === 0) {
+        this.unmarkedSquares--;
+      } else if (value === 0) {
+        this.unmarkedSquares++;
+      }
+
+      this.board[i][k] = value;
+
+      console.log(this.unmarkedSquares);
+
+      if(this.unmarkedSquares === 0) {
+        this.checkIfSolved();
+      }
     }
 
     render() {
-        let gs = this.state.gameSize;
+        let gs = this.gameSize;
         let squareArr = [];
-        for(let i = 0; i < (gs*gs); i++) {
-          squareArr.push(<Square key={i} squareState='empty'/>);
+        for(let i = 0; i < gs; i++) {
+         let newRow = [];
+          for(let k = 0; k < gs; k++) {
+            newRow.push(0);
+            squareArr.push(<Square key={i*gs+k} row={i} col={k} squareState='empty' updateBoard={this.updateBoard} />);
+          }
+          this.board.push(newRow);
         }
         return (
           <div className={`container ${boardClasses[gs]}`}>
--- src/components/Square/Square.js
+++ src/components/Square/Square.js
@@ -5,18 +5,29 @@ let nextSquareState = {
     'empty' : 'blue',
     'blue' : 'orange',
     'orange' : 'empty'
-}
+};
+
+let stateToValue = {
+    'empty' : 0,
+    'blue' : 1,
+    'orange' : 2
+};
 
 class Square extends Component {
     constructor(props) {
         super(props);
-        this.state = {squareState: props.squareState}
+        this.state = { squareState: props.squareState }
+        this.row = props.row;
+        this.col = props.col;
+        this.updateBoard = props.updateBoard;
     }
 
     handleClick = () => {
-        this.setState(prevState => ({
-            squareState: nextSquareState[prevState.squareState]
-        }));
+        let newSquareState = nextSquareState[this.state.squareState];
+        this.setState({
+            squareState: newSquareState
+        });
+        this.updateBoard(this.row, this.col, stateToValue[newSquareState]);
     }
 
     render() {
