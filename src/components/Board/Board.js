7cd9955c6534262aac0c30d01da264376248c7cf
Philip Read <Philip.Read.000@gmail.com>
2018-03-29 00:50:32 -0500

    Add incremental solution updater

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -97,8 +97,9 @@ class Board extends Component {
       }
 
       this.setState(prevState => {
-        prevState.board[i][k] = value;
-        return prevState;
+        let newBoard = prevState.board;
+        newBoard[i][k] = value;
+        return {board: newBoard};
       });
 
       if(this.unmarkedSquares === 0) {
@@ -106,27 +107,66 @@ class Board extends Component {
       }
     }
 
+    isDouble(i1, k1, i2, k2) {
+      if([i1, k1, i2, k2].some(val => val < 0 || val >= this.gameSize)) {
+        return 0;
+      }
+      let val1 = this.tempBoard[i1][k1];
+      let val2 = this.tempBoard[i2][k2];
+      return val1 === val2 ? val1 : 0;
+    }
+
     fillTriples() {
-      for(let i = 0; i < this.state.board.length; i++) {
-        for(let k = 0; k < this.state.board.length; k++) {
-          let squareVal = this.state.board[i][k];
-          if(squareVal === 0) {
-            this.unmarkedSquares--;
-            this.setState(prevState => {
-              let newValue = Math.randomInt(1,3);
-              console.log(newValue);
-              prevState.board[i][k] = newValue;
-              return prevState;
-            });
+      let foundDouble = true;
+      while(foundDouble) {
+        foundDouble = false;
+        for(let i = 0; i < this.tempBoard.length; i++) {
+          for(let k = 0; k < this.tempBoard.length; k++) {
+            let squareVal = this.tempBoard[i][k];
+            if(squareVal !== 0) {
+              continue;
+            }
+            let doubleCordss = [ 
+              [i, k-1, i, k-2], //left double
+              [i-1, k, i-2, k], //up double 
+              [i, k+1, i, k+2], //right double
+              [i+1, k, i+2, k], //bottom double
+              [i, k-1, i, k+1], //horiz cross double
+              [i-1, k, i+1, k]  //verti cross double
+            ];
+            for(let doubleCords of doubleCordss) {
+              let doubleVal = this.isDouble(...doubleCords);
+              if(doubleVal > 0) {
+                foundDouble = true;
+                this.tempBoard[i][k] = doubleVal === 2 ? 1 : 2;
+                this.unmarkedSquares--;
+                this.moves.push({i:i, k:k, value:this.tempBoard[i][k]});
+                break;
+              }
+            }
           }
         }
       }
     }
 
     solve = () => {
-      while(this.unmarkedSquares > 0) {
-        this.fillTriples();
+      this.tempBoard = []
+      for(let row of this.state.board) {
+        this.tempBoard.push([...row]);
       }
+      this.moves = [];
+      this.fillTriples();
+      let nextMove = move => {
+        if(move === undefined) {
+          return;
+        }
+        this.setState(prevState => {
+          let newBoard = prevState.board;
+          newBoard[move.i][move.k] = move.value;
+          return {board: newBoard};
+        }, () => setTimeout(() => nextMove(this.moves.shift()), 500));
+      };
+      nextMove(this.moves.shift());
     }
 
     render() {
